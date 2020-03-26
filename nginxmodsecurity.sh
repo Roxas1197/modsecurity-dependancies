9de95c00f06a9557ca63d6ae1d6de5a461a9fae3
Roxas1197 <keyblader97@msn.com>
2020-03-25 21:48:01 -0500

    Create nginxmodsecurity.sh
    Just testing

---
+++ nginxmodsecurity.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+apt-get install -y apt-utils autoconf automake build-essential git libcurl4-openssl-dev libgeoip-dev liblmdb-dev libpcre++-dev libtool libxml2-dev libyajl-dev pkgconf wget zlib1g-dev
+git clone --depth 1 -b v3/master --single-branch https://github.com/SpiderLabs/ModSecurity
+cd ModSecurity
+git submodule init
+git submodule update
+./build.sh
+./configure
+make
+make install
